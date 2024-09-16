e709d0e1aba6bd5b464f026f1e7c580dfe19338c
Andrey <89622883100@mail.ru>
2024-09-16 14:45:01 +0300

    Re-write App.js without JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 const currentDate = new Date(Date.now());
-
+/*
 export const App = () => {
 	return (
 		<div className="App">
@@ -22,5 +23,35 @@ export const App = () => {
 				<p>{currentDate.getFullYear()}</p>
 			</header>
 		</div>
+	);
+};
+*/
+export const App = () => {
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: logo }),
+			createElement(
+				'p',
+				{},
+				'Edit ',
+				createElement('code', {}, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', {}, currentDate.getFullYear()),
+		),
 	);
 };
